c2d6a33171efc274ed02344744931c050556df2b
Pascal Rath <rath@oxl.at>
2024-09-28 20:43:04 +0200

    refactor dns-recon script, add whois lookup

--- dns/README.md
+++ dns/README.md
@@ -11,6 +11,7 @@ Requirements: `pip install -r requirements.txt`
 ```bash
 python3 main.py <DOMAIN>
 
+# example:
 python3 main.py google.com
 ```
 
@@ -26,3 +27,117 @@ cat results_<DOMAIN>.json | jq -r '.[] | .ip | .ip4 | .[]' | uniq
 # get all IPv4 PTRs
 cat results_<DOMAIN>.json | jq -r '.[] | .ptr | .ip4 | .[]' | uniq
 ```
+
+----
+
+## Output
+
+### Whois
+
+```json
+{
+    "domain_name": "oxl.at",
+    "registrar": "World4You Internet Services GmbH ( https://nic.at/registrar/61 )",
+    "name_servers": [
+        "ns1.world4you.at",
+        "ns2.world4you.at"
+    ],
+    "name": "Domain Admin",
+    "org": "World4You Internet Services GmbH",
+    "address": "Hafenstrasse 35",
+    "registrant_postal_code": "4020",
+    "city": "Linz",
+    "country": "Austria",
+    "phone": "<data not disclosed>",
+    "fax": "<data not disclosed>",
+    "updated_date": [
+        "2022-08-11 00:19:34",
+        "2019-01-17 15:19:57"
+    ],
+    "email": "<data not disclosed>"
+}
+```
+
+### Domains/IPs
+
+Note: If the target domain has a wildcard-record set, the DNS-lookup checks might overlook some generic pages as we ignore any record that is pointing to the same IPs as the wildcard-record does.
+
+```json
+{
+    "*.oxl.at": {
+        "ip": {
+            "ip4": [
+                "159.69.187.50"
+            ],
+            "ip6": [
+                "2a01:4f8:c010:97b4::1"
+            ]
+        },
+        "ptr": {
+            "ip4": [
+                "lb01.c.oxl.at."
+            ],
+            "ip6": []
+        }
+    },
+    "lb01.c.oxl.at": {
+        "ip": {
+            "ip4": [
+                "159.69.187.50"
+            ],
+            "ip6": [
+                "2a01:4f8:c010:97b4::1"
+            ]
+        },
+        "ptr": {
+            "ip4": [
+                "lb01.c.oxl.at."
+            ],
+            "ip6": []
+        }
+    },
+    "__NS": [
+        "ns1.world4you.at.",
+        "ns2.world4you.at."
+    ],
+    "__MX": [
+        "1 SMTP.GOOGLE.COM."
+    ],
+    "__TXT": [
+        "\"v=spf1 mx include:_spf.oxl.at -all\"",
+        "\"google-site-verification=FLcbs-psv67SmmFyw8NQvz4iDKWPHxIadE6C4qhC2_Y\""
+    ],
+    "static.220.170.181.135.clients.your-server.de": {
+        "ip": {
+            "ip4": [
+                "135.181.170.220"
+            ],
+            "ip6": []
+        },
+        "ptr": {
+            "ip4": [
+                "static.220.170.181.135.clients.your-server.de."
+            ],
+            "ip6": []
+        }
+    },
+    "d51d74.mail.host-svc.com": {
+        "ip": {
+            "ip4": [
+                "49.13.245.115"
+            ],
+            "ip6": []
+        },
+        "ptr": {
+            "ip4": [
+                "d51d74.mail.host-svc.com."
+            ],
+            "ip6": []
+        }
+    },
+    "__DMARC": [
+        "\"v=DMARC1; p=reject; rua=mailto:dmarc@mail.host-svc.com; ruf=mailto:dmarc@mail.host-svc.com; aspf=s; adkim=s;\""
+    ]
+}
+```
+
--- dns/main.py
+++ dns/main.py
@@ -3,212 +3,81 @@
 from pathlib import Path
 from sys import argv
 from threading import Thread, Lock
-from time import sleep
+from time import sleep, time
 from json import dumps as json_dumps
 
-from dns.resolver import Resolver, NoAnswer, NXDOMAIN
+from dns.resolver import Resolver, NoAnswer, NXDOMAIN, LifetimeTimeout, NoNameservers
 from dns.exception import SyntaxError
+from whois import whois
 
 BASE_DIR = Path(__file__).parent.resolve()
 
 NAMESERVERS = ['1.1.1.1']
 WORDLIST = f'{BASE_DIR}/subdom-5k.txt'
 WILDCARD_RANDOM = 'ksdljfdlsnesfel3498394ßskeöskfölsjefk'
-THREADS = 10
+THREADS = 50
 FOLLOW_OTHER = False  # if ptrs/spf points to other parent-domains should be scanned (1-layer deep)
-
 TARGET = argv[1]
 TARGET_BASE = TARGET.rsplit('.', 1)[0]
-dns_resolver = Resolver(configure=False)
-dns_resolver.nameservers = NAMESERVERS
 
 
-def dns_lookup(dns: str) -> tuple[bool, dict]:
-    ips = {}
-    try:
-        ips['ip4'] = [r.to_text() for r in dns_resolver.resolve(dns, 'A')]
+class DNSRecon:
+    def __init__(self):
+        self.lock = Lock()
+        self.results = {}
+        self.dns = Resolver(configure=False)
+        self.dns.nameservers = NAMESERVERS
+        self.wildcard_exists = False
+        self.wildcard_ips = {}
+        self.start_time = time()
 
-    except (NoAnswer, NXDOMAIN):
-        ips['ip4'] = []
-
-    try:
-        ips['ip6'] = [r.to_text() for r in dns_resolver.resolve(dns, 'AAAA')]
-
-    except (NoAnswer, NXDOMAIN):
-        ips['ip6'] = []
-
-    return (len(ips['ip4']) > 0 or len(ips['ip6']) > 0), ips
-
-
-def ptr_lookups(ips: list[str]) -> list:
-    ptrs = []
-
-    for ip in ips:
+    def run(self):
+        self._process_basic_records()
+        self.wildcard_exists, self.wildcard_ips = self._check_for_wildcard()
         try:
-            ptrs.extend([p.to_text() for p in dns_resolver.resolve_address(ip)])
+            self._process_wordlist()
 
-        except (NoAnswer, NXDOMAIN, SyntaxError):
-            continue
+        except KeyboardInterrupt:
+            print()
+            print('WARNING: SCAN INTERRUPTED')
+            pass
 
-    return ptrs
+        self._save_results()
+        print('DONE')
 
+    def _save_results(self):
+        print('SAVING INFORMATION')
 
-def ptr_lookup_ips(ips: dict) -> dict:
-    return {
-        'ip4': ptr_lookups(ips['ip4']),
-        'ip6': ptr_lookups(ips['ip6']),
-    }
+        with open(f'whois_{TARGET}.json', 'w', encoding='utf-8') as f:
+            f.write(json_dumps(whois(TARGET), indent=4, default=str))
 
+        with open(f'results_{TARGET}.json', 'w', encoding='utf-8') as f:
+            f.write(json_dumps(self.results, indent=4))
 
-def subdomain(sub: str) -> str:
-    return f'{sub.strip()}.{TARGET}'
-
-
-def allow_follow(dom: str) -> bool:
-    return FOLLOW_OTHER or dom.find(TARGET_BASE) != -1  # allow follow to related domains
-
-
-def parse_spf(txt_entries: list[str]) -> list:
-    for e in txt_entries:
-        if e.find('v=spf1') != -1:
-            spf_domains = []
-
-            for p in e.split(' '):
-                try:
-                    pk, pv = p.split(':', 1)
-
-                except ValueError:
-                    continue
-
-                if pk in ['ip4', 'ip6']:
-                    spf_domains.extend(ptr_lookups([pv]))
-
-                elif pk in ['a']:
-                    spf_domains.append(pv)
-
-                elif pk in ['include', 'redirect']:
-                    # recurse if we should follow the lead
-                    if not allow_follow(pv):
-                        continue
-
-                    try:
-                        spf_domains.extend(parse_spf(
-                            [r.to_text() for r in dns_resolver.resolve(pv, 'TXT')]
-                        ))
-
-                    except (NoAnswer, NXDOMAIN):
-                        pass
-
-            return spf_domains
-
-    return []
-
-
-def main():
-    result_lock = Lock()
-    results = {}
-
-    def _get_ips_if_relevant(dom: str, wildcard_filter: bool = True) -> (dict, None):
-        if dom in results:
-            return
-
-        exists, ips = dns_lookup(dom)
-        if not exists:
-            return
-
-        if wildcard_filter and wildcard_exists and ips['ip4'] == wildcard_ips['ip4'] and ips['ip6'] == wildcard_ips['ip6']:
-            return
-
-        return ips
-
-    def _check_ptrs(ptrs: dict, ptr_domain: str):
-        # todo: create cleaner approach for recursive ptr processing
-        for ipp in ['ip4', 'ip6']:
-            for d in ptrs[ipp]:
-                d = d[:-1]
-                if d != ptr_domain and allow_follow(d) and d not in results:
-                    ips2 = _get_ips_if_relevant(d, False)
-                    if ips2 is None:
-                        continue
-
-                    print('FOUND:', d, '(PTR)')
-                    ptrs2 = ptr_lookup_ips(ips2)
-
-                    with result_lock:
-                        results[d] = {'ip': ips2, 'ptr': ptrs2}
-
-    def _lookup(dom: str):
-        dom = dom.lower()
-        if dom.endswith('.'):
-            dom = dom[:-1]
-
-        ips = _get_ips_if_relevant(dom)
-        if ips is None:
-            return
-
-        print('FOUND:', dom)
-
-        ptrs = ptr_lookup_ips(ips)
-
-        with result_lock:
-            results[dom] = {'ip': ips, 'ptr': ptrs}
-
-        _check_ptrs(ptrs, dom)
-
-    def _lookup_sub(word: str):
-        _lookup(subdomain(word))
-
-    # WILDCARD records
-    wildcard_exists, wildcard_ips = dns_lookup(subdomain(WILDCARD_RANDOM))
-    print('HAS WILDCARD:', wildcard_exists)
-    if wildcard_exists:
-        wildcard_ptrs = ptr_lookup_ips(wildcard_ips)
-        results[subdomain('*')] = {'ip': wildcard_ips, 'ptr': wildcard_ptrs}
-        _check_ptrs(wildcard_ptrs, subdomain('*'))
-
-        print('WARNING: We will ignore all records that match the wildcard. Some generic ones might be missing!')
+    def _process_wordlist(self):
         print()
+        print('STARTING SUBDOMAIN PROBES')
 
-    # BASIC records
-    results['__NS'] = [r.to_text() for r in dns_resolver.resolve(TARGET, 'NS')]
+        with open(WORDLIST, 'r', encoding='utf-8') as f:
+            wordlist = f.readlines()
 
-    try:
-        results['__MX'] = [r.to_text() for r in dns_resolver.resolve(TARGET, 'MX')]
-
-    except (NoAnswer, NXDOMAIN):
-        pass
-
-    try:
-        results['__TXT'] = [r.to_text() for r in dns_resolver.resolve(TARGET, 'TXT')]
-        print('PARSING SPF')
-        for sd in parse_spf(results['__TXT']):
-            _lookup(sd)
-
-    except (NoAnswer, NXDOMAIN):
-        pass
-
-    try:
-        results['__DMARC'] = [r.to_text() for r in dns_resolver.resolve(subdomain('_dmarc'), 'TXT')]
-
-    except (NoAnswer, NXDOMAIN):
-        pass
-
-    # MAIN CHECKS
-    print()
-    print('STARTING SUBDOMAIN PROBES')
-
-    with open(WORDLIST, 'r', encoding='utf-8') as f:
-        wordlist = f.readlines()
-
-    try:
         batch = 0
         while batch * THREADS < len(wordlist):
             threads = []
             for i in range(THREADS):
+                idx = (batch * THREADS) + i
+                if idx > len(wordlist) - 1:
+                    break
+
                 threads.append(Thread(
-                    target=_lookup_sub,
-                    kwargs={'word': wordlist[(batch * THREADS) + i]},
+                    target=self._lookup_sub,
+                    kwargs={'word': wordlist[idx]},
                 ))
+                if idx % 500 == 0 and idx != 0:
+                    print(
+                        f'INFO: {int((100 / len(wordlist)) * idx)}% ({idx}/{len(wordlist)}) '
+                        f'in {int(time()-self.start_time)}s'
+                    )
 
             for t in threads:
                 t.start()
@@ -220,15 +89,203 @@ def main():
 
             batch += 1
 
-    except KeyboardInterrupt:
-        print()
-        print('WARNING: SCAN INTERRUPTED')
-        pass
+    def _process_basic_records(self):
+        try:
+            self.results['__NS'] = [r.to_text() for r in self.dns.resolve(TARGET, 'NS')]
 
-    with open(f'results_{TARGET}.json', 'w', encoding='utf-8') as f:
-        f.write(json_dumps(results, indent=4))
+        except NXDOMAIN:
+            print(f"ERROR: The domain '{TARGET}' is not resolvable! Check it for typos!")
+            exit(1)
 
-    print('DONE')
+        try:
+            self.results['__MX'] = [r.to_text() for r in self.dns.resolve(TARGET, 'MX')]
+
+        except (NoAnswer, NXDOMAIN):
+            pass
+
+        try:
+            self.results['__TXT'] = [r.to_text() for r in self.dns.resolve(TARGET, 'TXT')]
+            print('PARSING SPF')
+            for sd in self._parse_spf(self.results['__TXT']):
+                self._lookup(sd)
+
+        except (NoAnswer, NXDOMAIN):
+            pass
+
+        try:
+            self.results['__DMARC'] = [r.to_text() for r in self.dns.resolve(self._subdomain('_dmarc'), 'TXT')]
+
+        except (NoAnswer, NXDOMAIN):
+            pass
+
+    def _check_for_wildcard(self):
+        wildcard_exists, wildcard_ips = self._name_lookup(self._subdomain(WILDCARD_RANDOM))
+        print('HAS WILDCARD:', wildcard_exists)
+        if wildcard_exists:
+            wildcard_ptrs = self._ptr_lookup_ips(wildcard_ips)
+            self.results[self._subdomain('*')] = {'ip': wildcard_ips, 'ptr': wildcard_ptrs}
+            self._check_ptrs(wildcard_ptrs, self._subdomain('*'))
+
+            print('WARNING: We will ignore all records that match the wildcard. Some generic ones might be missing!')
+            print()
+
+        return wildcard_exists, wildcard_ips
+
+    def _name_lookup(self, dns: str) -> tuple[bool, dict]:
+        ips = {}
+        try:
+            retry = 0
+            while True:
+                try:
+                    ips['ip4'] = [r.to_text() for r in self.dns.resolve(dns, 'A')]
+                    break
+
+                except (LifetimeTimeout, NoNameservers):
+                    if retry >= 5:
+                        raise NXDOMAIN
+
+                    retry += 1
+                    continue
+
+
+
+        except (NoAnswer, NXDOMAIN):
+            ips['ip4'] = []
+
+        try:
+            retry = 0
+            while True:
+                try:
+                    ips['ip6'] = [r.to_text() for r in self.dns.resolve(dns, 'AAAA')]
+                    break
+
+                except (LifetimeTimeout, NoNameservers):
+                    if retry >= 5:
+                        raise NXDOMAIN
+
+                    retry += 1
+                    continue
+
+        except (NoAnswer, NXDOMAIN):
+            ips['ip6'] = []
+
+        return (len(ips['ip4']) > 0 or len(ips['ip6']) > 0), ips
+
+    def _ptr_lookups(self, ips: list[str]) -> list:
+        ptrs = []
+
+        for ip in ips:
+            try:
+                ptrs.extend([p.to_text() for p in self.dns.resolve_address(ip)])
+
+            except (NoAnswer, NXDOMAIN, SyntaxError):
+                continue
+
+        return ptrs
+
+
+    def _ptr_lookup_ips(self, ips: dict) -> dict:
+        return {
+            'ip4': self._ptr_lookups(ips['ip4']),
+            'ip6': self._ptr_lookups(ips['ip6']),
+        }
+
+
+    @staticmethod
+    def _subdomain(sub: str) -> str:
+        return f'{sub.strip()}.{TARGET}'
+
+    @staticmethod
+    def _allow_follow(dom: str) -> bool:
+        return FOLLOW_OTHER or \
+            dom.find(TARGET_BASE) != -1  # allow follow to related domains
+
+    def _parse_spf(self, txt_entries: list[str]) -> list:
+        for e in txt_entries:
+            if e.find('v=spf1') != -1:
+                spf_domains = []
+
+                for p in e.split(' '):
+                    try:
+                        pk, pv = p.split(':', 1)
+
+                    except ValueError:
+                        continue
+
+                    if pk in ['ip4', 'ip6']:
+                        spf_domains.extend(self._ptr_lookups([pv]))
+
+                    elif pk in ['a']:
+                        spf_domains.append(pv)
+
+                    elif pk in ['include', 'redirect']:
+                        # recurse if we should follow the lead
+                        if not self._allow_follow(pv):
+                            continue
+
+                        try:
+                            spf_domains.extend(self._parse_spf(
+                                [r.to_text() for r in self.dns.resolve(pv, 'TXT')]
+                            ))
+
+                        except (NoAnswer, NXDOMAIN):
+                            pass
+
+                return spf_domains
+
+        return []
+
+    def _get_ips_if_relevant(self, dom: str, wildcard_filter: bool = True) -> (dict, None):
+        if dom in self.results:
+            return
+
+        exists, ips = self._name_lookup(dom)
+        if not exists:
+            return
+
+        if wildcard_filter and self.wildcard_exists and \
+                ips['ip4'] == self.wildcard_ips['ip4'] and ips['ip6'] == self.wildcard_ips['ip6']:
+            return
+
+        return ips
+
+    def _check_ptrs(self, ptrs: dict, ptr_domain: str):
+        # todo: create cleaner approach for recursive ptr processing
+        for ipp in ['ip4', 'ip6']:
+            for d in ptrs[ipp]:
+                d = d[:-1]
+                if d != ptr_domain and self._allow_follow(d) and d not in self.results:
+                    ips2 = self._get_ips_if_relevant(d, False)
+                    if ips2 is None:
+                        continue
+
+                    print('FOUND:', d, '(PTR)')
+                    ptrs2 = self._ptr_lookup_ips(ips2)
+
+                    with self.lock:
+                        self.results[d] = {'ip': ips2, 'ptr': ptrs2}
+
+    def _lookup(self, dom: str):
+        dom = dom.lower()
+        if dom.endswith('.'):
+            dom = dom[:-1]
+
+        ips = self._get_ips_if_relevant(dom)
+        if ips is None:
+            return
+
+        print('FOUND:', dom)
+
+        ptrs = self._ptr_lookup_ips(ips)
+
+        with self.lock:
+            self.results[dom] = {'ip': ips, 'ptr': ptrs}
+
+        self._check_ptrs(ptrs, dom)
+
+    def _lookup_sub(self, word: str):
+        self._lookup(self._subdomain(word))
+
 
 if __name__ == '__main__':
-    main()
+    DNSRecon().run()
